066418f6dde5333768dda66d26ba41a89f6da509
Jiang  Ziping <jiangz9@wayland-2020.private.dns.zone>
2023-05-13 23:29:33 +0100

    add bash scripts for training

---
+++ bash/prune_l1.sh
@@ -0,0 +1,13 @@
+#$ -S /bin/bash
+
+#qsub train.sh --dataset cifar10 --net vgg16 --project prune_l1 --name benchmark --num_epoch 120 --train_mode std --lr_scheduler milestones  --batch_size 256  --activation ReLU  --npbar
+qsub train.sh --dataset cifar10 --net VGG16 --project prune_l1 --name benchmark --num_epoch 120
+
+for method in 0  3 4 5
+do
+	for amount in 0.01 0.02
+	do
+	qsub train.sh --dataset cifar10 --net vgg16 --project prune_l1 --name l1_${method}_${total_amount} --num_epoch 120 --amount ${amount} \
+        --lr_scheduler milestones --method ${method} --activation ReLU --npbar --amount_setting 0
+    done
+done
